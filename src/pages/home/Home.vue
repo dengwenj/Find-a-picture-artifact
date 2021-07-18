9ff6731452ec54d3cdfe39da47274f6a38f09885
xiaodeng <927561237@qq.com>
2021-07-18 23:32:03 +0800

    项目搭建tabbar

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <view class="content">首页</view>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="scss"></style>
---
+++ src/pages/horizontal/Horizontal.vue
@@ -0,0 +1,9 @@
+<template>
+  <view class="content">图片</view>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="scss"></style>
